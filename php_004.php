b34df19252fc2bed09a92b33c28be61923f712dc
FerFlower <diegoquispeapaza45@gmail.com>
2023-04-05 18:31:44 -0500

    00404

---
+++ php_004.php
@@ -0,0 +1,12 @@
+<html lang="es">
+<?php
+
+$Nombre = "Diego Fernando";
+$Apellido = "Quispe Apaza" ;
+$edad = 18;
+$Centro_de_estudio = "Senati";
+$Trabajo = null;
+ 
+echo  "Mi nombre es :".$Nombre."<br>Mi apellido es : " .$Apellido."<br>Mi edad es :".$edad."<br>Mi sentro de estudio es : ".$Centro_de_estudio."<br>Mi trabajo es : ".$Trabajo;
+
+?>
