3eaa9b4121ba51b4c35718be8c11e36158b6cadd
FerFlower <diegoquispeapaza45@gmail.com>
2023-04-04 19:37:54 -0500

    ooo4

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-echo "<p>Hola mundo</p>";
-
+//echo "<p>Hola mundo</p>esto es un juego ";
+$Mi_numero = 16;
+if($Numero < 18){
+    echo "Eres menor de edad <br>";
+}else if ($Numero >= 18 && $Numero<68){
+    echo "Eres ";
+}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    
+
+</body>
+</html>
--- php_001.php
+++ php_001.php
@@ -1,4 +1,16 @@
-<?php
-echo "Hola mundo";
+<?php 
+$Deuda = 240; 
+$Interes; 
 
+if ($Deuda <= 100) { 
+    $Interes = 15; 
+} elseif ($Deuda >=101 && $Deuda <=400){ 
+    $Interes= 17; 
+} elseif($deuda >=4001 && $Deuda <= 1000){
+    $Interes= 20;
+}else{
+    $Interes= 25;
+}
+$total = $Deuda + ($Deuda * ($Interes/100)); 
+echo "Tu deuda es de s/.".$Deuda." + ".$Interes."% deberas de pagar un total de ". $total . " soles";
 ?>
