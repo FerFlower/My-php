4f1099bbd72391c7390864ec17b39654b8871e5f
FerFlower <diegoquispeapaza45@gmail.com>
2023-04-11 20:11:36 -0500

    2323

--- php_010.php
+++ php_010.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="hola.css">
+<link rel="stylesheet" href="diseño.css">
 
 <?php  
 
@@ -6,13 +6,58 @@ if($_POST){
     $Numero1= $_POST['1'];
     $Numero2=$_POST['2'];
     $Numero3=$_POST['3'];
+notas($Numero1,$Numero2,$Numero3);
 
-    //echo "Tu primer numero es : ".$Numero1."Tu segundo numero es : ".$Numero2."Tu terser numero es : ".$Numero3;
 
-if($Numero1 == 12 == $Numero2 == $Numero3){
-echo "Te salbastes y aprobestes apenas"
 }
+function notas($n1, $n2 ,$n3){
+    $pro=($n1+$n2+$n3)/3;
+    $pase =true;
+    define("c1",20);
+    define("c2",10);
 
+    if($n1>20 ){
+        Echo "<h3><br><br>Tu Primer numero es inbalido</h3>";
+        $pase = false;
+
+    }
+    if($n2>20 ){
+        Echo "<h3><br><br>Tu Segundi numero es inbalido</h3>";
+        $pase = false;
+    }
+    if($n3>20 ){
+        Echo "<h3><br><br>Tu Terser numero es inbalido</h3>";
+        $pase = false;
+    }
+    if($pro>20 ){
+        echo "<h3><br><br>La nota es inbalida por que el promedio maximo es 20</h3>";
+        $pase = false;
+    }
     
-}
+if($pase){
+    if(c2>= $pro){
+        Echo "<h3><br>Has desaprovado</h3>";
+        if($pro<=5){
+           echo "<br>Tu nota es una 'D'</h3>";
+        }elseif ($pro<=10 && $pro>5) {
+            echo "<h3><br>Tu nota es una 'C'</h3>";
+        } else {
+            # code...
+        }
+        
+    }elseif(c1>= $pro && c2 < $pro){
+        Echo "<h3><br>Has aprovado";
+        if($pro>10 && $pro<=15){
+            echo "<h3><br>Tu nota es una 'B'</h3>";
+         }elseif ($pro<=20 && $pro>15) {
+             echo "<h3><br>Tu nota es una 'A'</h3>";
+         } else {
+         }
+    }else{
+        echo "<h3><br><br>La nota es inbalida por que el promedio maximo es 20</h3>";
+    }
+    $proi = round($pro, 2);
+    echo "<h3><br><br>Tu promedio es :".$proi."</h3>";
+   
+}}
 ?>
